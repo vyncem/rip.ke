b0ae2df8b8eb8b5f93b4def75431b5049116cd77
vyncem <vyncem@gmail.com>
2024-10-28 20:48:28 +0300

    Fix pay button

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
+
 class MpesasController < ApplicationController
   def index
-    @mpesas = params || []
+    @mpesas = params.merge(call_mpesa) || []
 
     Rails.logger.info(@mpesas)
+
+    redirect_to homes_url, notice: @mpesas
   end
   alias create index
+
+  private
+
+  def call_mpesa
+    JSON.parse(
+      MpesaService.new(amount: 1, payer: ENV.fetch('DARAJA_PAYER', nil), payee: ENV.fetch('DARAJA_SHORT_CODE', nil)).call
+    )
+  end
 end
